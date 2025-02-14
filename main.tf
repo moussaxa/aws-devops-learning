9ad79407d042cb32f5469b76ce93f8d675ebdbf4
xavier <xdmoussa3@gmail.com>
2025-02-13 22:56:48 -0500

    Added s3 backend with dynamodb lock

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region  = "us-east-1"
-    profile = "Xavier"
+    profile = "default"
 }
 
 resource "aws_s3_bucket" "Xavier-bucket" {
@@ -9,5 +9,28 @@ resource "aws_s3_bucket" "Xavier-bucket" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
   }
 }
