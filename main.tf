97f6effa14308af9aa95af1a5461f001b9a3a5e1
xavier <xdmoussa3@gmail.com>
2025-02-26 23:11:20 -0500

    created locals block

--- main.tf
+++ main.tf
@@ -2,51 +2,53 @@ provider "aws" {
     region  = "us-east-1"
     profile = "default"
 }
+locals {
+  name = "xavier"
+}
+module "ec2" {
+  source = "./modules/ec2"
+  name = "${local.name}"
+  }
 
-# module "ec2" {
-#   source = "./modules/ec2"
-#   name = "xavier-ec2"
-#   }
+module "s3" {
+  source = "./modules/s3"
+  name = "${local.name}"
+  }
+  
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-# module "s3" {
-#   source = "./modules/s3"
-#   name = "xavier-s3-bucket-test"
-# }
+  name = "${local.name}"
+  cidr = "10.0.0.0/16"
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-#   name = "my-vpc"
-#   cidr = "10.0.0.0/16"
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
-#   azs             = ["us-east-1a", "us-east-1b"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 
-#   enable_nat_gateway = true
-#   enable_vpn_gateway = false
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
-#   tags = {
-#     Terraform = "true"
-#     Environment = "dev"
-#   }
-# }
+  name = "${local.name}"
 
-# module "ec2_instance" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0a75e249fda05175e"]
+  subnet_id              = "subnet-0ed0ff68f1c8add88"
 
-#   name = "my-ec2"
-
-#   instance_type          = "t2.micro"
-#   monitoring             = true
-#   vpc_security_group_ids = ["sg-0a75e249fda05175e"]
-#   subnet_id              = "subnet-0ed0ff68f1c8add88"
-
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#   }
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 resource "null_resource" "example" {
   provisioner "local-exec" {
     command = "echo 'Hello, Terraform!'"
